28b1468712b55b91de766fbf23b792debc996fde
Sk Nayeem Ur Rahman <133092904+nayeemdiu@users.noreply.github.com>
2023-05-15 09:58:52 +0600

    Add files via upload

---
+++ swith_case.dart
@@ -0,0 +1,42 @@
+void main() {
+  var days = 7;
+  var month = 8;
+
+  switch (days) {
+    case 1:
+      {
+        print("one is satarday");
+      }
+
+      break;
+    case 2:
+      {
+        print("one is sunday");
+      }
+
+      break;
+    case 3:
+      {
+        print("Three is Monday");
+      }
+
+      break;
+    case 4:
+      {
+        print("Four is Tharstday");
+      }
+
+      break;
+    case 5:
+      {
+        print("Five is Wethnesday");
+      }
+
+      break;
+    default:
+      {
+        print("not matched");
+      }
+      break;
+  }
+}
